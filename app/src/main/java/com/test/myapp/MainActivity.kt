c26ce7c379c23f41d46f5c4ac5677cc470c31354
hefeng <hefeng02@agora.io>
2023-08-28 14:30:21 +0800

    use mysubmodule1 && mysubmodule1

--- app/src/main/java/com/test/myapp/MainActivity.kt
+++ app/src/main/java/com/test/myapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.test.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.test.mysubmodule1.SubModule1
+import com.test.mysubmodule2.SubModule2
 
 class MainActivity : AppCompatActivity() {
+    val m1 = SubModule1()
+    val m2 = SubModule2()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("MainActivity", "m1 name: ${m1.getSubModuleName()}")
+        Log.e("MainActivity", "m2 name: ${m2.getSubModuleName()}")
+
     }
 }
