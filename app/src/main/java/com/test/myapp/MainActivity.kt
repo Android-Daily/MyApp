ed3e9384cdd00b55299fdff51144a77c53f1517e
hefeng <hefeng02@agora.io>
2023-10-08 11:44:02 +0800

    add module3

--- app/src/main/java/com/test/myapp/MainActivity.kt
+++ app/src/main/java/com/test/myapp/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.util.Log
 import com.test.mysubmodule1.SubModule1
 import com.test.mysubmodule2.SubModule2
+import com.test.mysubmodule3.SubModule3
 
 class MainActivity : AppCompatActivity() {
     val m1 = SubModule1()
     val m2 = SubModule2()
+    val m3 = SubModule3()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         Log.e("MainActivity", "m1 name: ${m1.getSubModuleName()}")
         Log.e("MainActivity", "m2 name: ${m2.getSubModuleName()}")
+        Log.e("MainActivity", "m3 name: ${m3.getSubModuleName()}")
 
     }
 }
